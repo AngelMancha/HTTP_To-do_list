31938bd6f1b14c67a927477c51d8036d2909815f
AngelMancha <100451302@alumnos.uc3m.es>
2023-03-16 22:01:35 +0100

    Función eliminar al hacer swipe

--- www/script.js
+++ www/script.js
@@ -1,3 +1,11 @@
+let startX = 0;
+let endX = 0;
+let startTime = 0;
+
+const TIME_THRESHOLD = 200;
+const SPACE_THRESHOLD = 100;
+
+
 
 //lista de tareas
 let todos = [
@@ -8,7 +16,8 @@ let todos = [
 
 const tablaTareas = document.getElementById('tabla-tareas');
 
-displayTodos(); 
+displayTodos();
+detectar_swipe(); 
 
 function displayTodos() {
     tablaTareas.innerHTML = '';
@@ -33,14 +42,68 @@ const addTodo = () => {
 
     newTodo.value = '';
     displayTodos();
+    detectar_swipe();
     
   
 }
 
-//función que elimina una tarea en concreto de la lista de tareas
+/*función que elimina una tarea en concreto de la lista de tareas
 function deleteTodo(id) {
     todos = todos.filter(function(todo) {
         return todo.id !== id;
     });
     displayTodos();
+    
+}
+*/
+
+
+const addButton = document.querySelector("#fab-add");
+
+addButton.addEventListener("touchend", addTodo);
+
+
+function detectar_swipe(){
+        const items = document.querySelectorAll("td");
+
+        items.forEach(item => {
+        item.addEventListener("touchstart", e => {
+            e.preventDefault();
+            e.target.classList.remove("swiped");
+            startX = e.targetTouches[0].screenX;
+            startTime = e.timeStamp;
+        }, { passive: false });
+
+        item.addEventListener("touchmove", e => {
+            e.preventDefault();
+            endX = e.changedTouches[0].screenX;
+        }, { passive: false });
+
+        item.addEventListener("touchend", e => {
+            e.preventDefault();
+            endTime = e.timeStamp;
+            endX = e.changedTouches[0].screenX;
+            if (endTime - startTime < TIME_THRESHOLD && endX - startX > SPACE_THRESHOLD) {
+            delete_tarea(e.target);
+            eliminar_tarea_de_la_lista(e.target);
+            }
+        });
+        });
+}
+
+//eliminar elemento td
+function delete_tarea(element) {
+
+    element.parentNode.removeChild(element);
+    
+}
+
+function eliminar_tarea_de_la_lista(element){
+    const id = element.parentNode.dataset.id;
+    todos = todos.filter(todo => todo.id !== parseInt(id));
+    displayTodos();
+}
+function handleSwipe(element) {
+  element.classList.add("swiped");
+
 }
